392f9e5a4a48acd01dcce8c5ada007ffbed021a8
Michael Bishop <cleverca22@gmail.com>
2025-03-14 13:56:52 -0300

    fix bss clearing

--- arch/ppc64/arch.c
+++ arch/ppc64/arch.c
@@ -5,6 +5,13 @@ void arch_idle(void) {
     asm volatile("nop");
 }
 
+void clear_bss(void) {
+  extern uint8_t __bss_start, __bss_end;
+  for (uint8_t *t = &__bss_start; t < &__bss_end; t++) {
+    *t = 0;
+  }
+}
+
 void arch_early_init(void) {
 }
 
@@ -13,4 +20,8 @@ void arch_init(void) {
 
 void arch_chain_load(void *entry, ulong arg0, ulong arg1, ulong arg2, ulong arg3) {
   panic("unimplemented");
+}
+
+void arch_clean_cache_range(addr_t start, size_t len) {
+  //PANIC_UNIMPLEMENTED;
 }
