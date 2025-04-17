4df95072e4a925509707df0b7b183a0c84a96c54
Michael Bishop <cleverca22@gmail.com>
2025-04-17 03:00:05 -0300

    it partially boots under xell

--- project/lk-ppc.mk
+++ project/lk-ppc.mk
@@ -2,12 +2,12 @@ LOCAL_DIR := $(GET_LOCAL_DIR)
 
 TARGET := xenon-ppc64-stage1
 
-MODULES += app/shell
+#MODULES += app/shell
 #MODULES += app/tests
-MODULES += lib/debugcommands
-MODULES += lib/gfx
+#MODULES += lib/debugcommands
+#MODULES += lib/gfx
 #MODULES += lib/gfxconsole
 
-DEBUG := 2
-WITH_TESTS := false
-
+DEBUG := 0
+# WITH_TESTS := false
+WITH_LINKER_GC := true
